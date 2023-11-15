6f26f8c50a2458fea2f39c2fc0f64a897b41d4ee
Hamid Musayev <hemidvsmusayev@gmail.com>
2023-11-15 11:27:17 +0400

    required keyword

--- Api/Item.cs
+++ Api/Item.cs
@@ -3,7 +3,7 @@
     public class Item
     {
         public int ItemId { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
+        public required string Name { get; set; }
+        public required decimal Price { get; set; }
     }
 }
